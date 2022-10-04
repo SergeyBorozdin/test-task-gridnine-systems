3a7d7d23b3536a2d0c804e2afeca84d1e3426c66
Sergey Borozdin <borozdinsm@yandex.ru>
2022-10-05 00:50:15 +0300

    added methods sign

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,12 +1,27 @@
 package com.gridnine.testing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        for (Object obj : FlightBuilder.createFlights()) {
-            System.out.println(obj);
-        }
+//        for (Object obj : FlightBuilder.createFlights()) {
+//            System.out.println(obj);
+//        }
 
+    }
+
+    public static List<Segment> getExcludeSegmentBeforeTime () {
+        return new ArrayList<>();
+    }
+
+    public static List<Segment> getExcludeArrivalsBeforeDeparture () {
+        return new ArrayList<>();
+    }
+
+    public static List<Segment> getExcludeTransferMore2hours () {
+        return new ArrayList<>();
     }
 }
